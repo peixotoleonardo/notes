cd8903266f14978cf5b10d65d77dd5e7cec3cbeb
Leonardo Peixoto <leonardo.peixoto@fortics.com.br>
2023-11-14 07:55:03 -0300

    feat(node): add attention comment

--- node/assert/assert_and_ok.mjs
+++ node/assert/assert_and_ok.mjs
@@ -6,6 +6,13 @@
  */
 import assert from 'node:assert';
 
+/**
+ * Attention: don't use it in production, because
+ * when this function generates a message that will 
+ * be displayed, it use synchronous code.
+ * 
+ * https://github.com/nodejs/node/blob/eb46e09570c9262ccf97e3b11c2ad52de29a3a37/lib/assert.js#L342
+ */
 assert.doesNotThrow(() => assert(true));
 assert.doesNotThrow(() => assert.ok(true));
 
